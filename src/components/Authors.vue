93b63f97b83312faffc6d85c292586c78338cb53
Benjamin Austin <bnjmn80@gmail.com>
2018-08-28 15:22:19 -0600

    Adding Authors.vue component

---
+++ src/components/Authors.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <Header />
+        <p>
+            Hooray for authors! 
+        </p>
+
+    </div>   
+
+</template>
+
+
+<script>
+import Header from './Header'
+
+export default {
+    name: "Authorslist",
+    components: {
+        Header
+    },
+    data(){
+        return {
+            authors: null
+        }
+    }//,
+    // methods: {
+    //     getData(){
+    //         fetch('http://localhost:9000/authors')
+    //         .then(response => {
+    //             console.log("the response from authors fetch: ", response);
+    //             return response.json();
+    //         })
+    //         .then( json => {
+    //             this.authors = json.authors;
+    //             console.log("here's what's in my data object for authors: ", this.authors);
+    //         });
+    //     }
+    // },    
+    // mounted() {
+    //     this.getData()
+    // }
+
+}
+
+</script>
+
+<style>
+
+
+</style>
