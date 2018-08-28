4e87389fe555e072265c37bdc3dfe43d3a9c7bf3
Benjamin Austin <bnjmn80@gmail.com>
2018-08-28 15:16:51 -0600

    Set up router and App.vue and Home.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+
+
 export default {
   name: 'App'
+  
 }
 </script>
 
@@ -18,6 +20,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Books from '@/components/Books'
+import Authors from '@/components/Authors'
 
 Vue.use(Router)
 
@@ -8,8 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/books',
+      name: 'Books',
+      component: Books
+    },
+    {
+      path: '/authors',
+      name: 'Authors',
+      component: Authors
     }
   ]
 })
